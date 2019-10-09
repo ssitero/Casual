dcd88da3f6e4ed6c1ff5d89e04e1b67b02dc7426
ssitero <42685018+ssitero@users.noreply.github.com>
2019-10-09 10:56:24 -0400

    PauseMenu done

---
+++ Chubb/Assets/Scripts/Pausing.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pausing : MonoBehaviour
+{
+  public static bool GameIsPaused = false;
+  public GameObject PauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.P)){
+
+      if (GameIsPaused){
+      Resume();
+      }
+      else {
+      Pause();
+      }
+      }
+    }
+
+    public void Resume (){
+    PauseMenuUI.SetActive(false);
+    Time.timeScale = 1f;
+    GameIsPaused = false;
+
+    }
+
+    void Pause(){
+
+    PauseMenuUI.SetActive(true);
+    Time.timeScale = 0f;
+    GameIsPaused = true;
+
+
+
+    }
+    public void quitmenu (){
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
