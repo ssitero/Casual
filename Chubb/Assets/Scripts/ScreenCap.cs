02d8ab926acf090cb29f13f634b1eab71f6f884b
Samuel <ssitero@knights.ucf.edu>
2019-10-20 23:43:14 -0400

    d

--- Chubb/Assets/Scripts/ScreenCap.cs
+++ Chubb/Assets/Scripts/ScreenCap.cs
@@ -6,15 +6,15 @@ using UnityEngine;
 public class ScreenCap: MonoBehaviour
 {
     Texture2D screenCap;
-    Texture2D border;
+   // Texture2D border;
     bool shot = false;
 
     // Use this for initialization
     void Start()
     {
         screenCap = new Texture2D(300, 200, TextureFormat.RGB24, false); // 1
-        border = new Texture2D(2, 2, TextureFormat.ARGB32, false); // 2
-        border.Apply();
+       // border = new Texture2D(2, 2, TextureFormat.ARGB32, false); // 2
+       // border.Apply();
     }
 
     // Update is called once per frame
@@ -29,11 +29,12 @@ public class ScreenCap: MonoBehaviour
 
     void OnGUI()
     {
+        /*
         GUI.DrawTexture(new Rect(200, 100, 300, 2), border, ScaleMode.StretchToFill); // top
         GUI.DrawTexture(new Rect(200, 300, 300, 2), border, ScaleMode.StretchToFill); // bottom
         GUI.DrawTexture(new Rect(200, 100, 2, 200), border, ScaleMode.StretchToFill); // left
         GUI.DrawTexture(new Rect(500, 100, 2, 200), border, ScaleMode.StretchToFill); // right
-
+*/
         if (shot)
         {
             GUI.DrawTexture(new Rect(10, 10, 60, 40), screenCap, ScaleMode.StretchToFill);
@@ -43,7 +44,7 @@ public class ScreenCap: MonoBehaviour
     IEnumerator Capture()
     {
         yield return new WaitForEndOfFrame();
-        screenCap.ReadPixels(new Rect(198, 98, 298, 198), 0, 0);
+        screenCap.ReadPixels(new Rect(500, 500, 500, 500), 0, 0);
         screenCap.Apply();
         shot = true;
     }
