01e000adc561e820c5f2016a3a05735bde3d1041
Samuel <ssitero@knights.ucf.edu>
2019-10-31 16:00:36 -0400

    Vertical slice is done

--- Chubb/Assets/Scripts/Pausing3.cs
+++ Chubb/Assets/Scripts/Pausing3.cs
@@ -5,40 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class Pausing3 : MonoBehaviour
 {
-  public static bool GameIsPaused = false;
-  public GameObject PauseMenuUI;
+    //  public static bool GameIsPaused = false;
+    //  public GameObject PauseMenuUI;
 
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetKeyDown(KeyCode.P)){
+        //  if (Input.GetKeyDown(KeyCode.P)){
 
-      if (GameIsPaused){
-      Resume();
-      }
-      else {
-      Pause();
-      }
-      }
+        //  if (GameIsPaused){
+        //  Resume();
+        //  }
+        //  else {
+        //  Pause();
+        //  }
+        //  }
     }
+    /*
+        public void Resume (){
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
 
-    public void Resume (){
-    PauseMenuUI.SetActive(false);
-    Time.timeScale = 1f;
-    GameIsPaused = false;
+        }
 
-    }
+        void Pause(){
 
-    void Pause(){
-
-    PauseMenuUI.SetActive(true);
-    Time.timeScale = 0f;
-    GameIsPaused = true;
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
 
 
 
-    }
-    public void quitmenu (){
-      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+        }
+        */
+    public void quitmenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
     }
 }
